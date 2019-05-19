949ed69b9112a136c91613215174773960e6edec
Aaron Lipman <alipman88@gmail.com>
2019-05-18 23:44:16 -0400

    minimize mysql queries with caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_poll
-      @poll = Poll.find_by(slug: params[:poll]) || Domain.find_by!(domain: request.host).poll
+      key = "#{ request.host }/#{ params[:poll] }"
+      Rails.cache.delete(key) if Rails.cache.read(key).nil?
+
+      @poll = Rails.cache.fetch key, expires_in: 10.minutes, race_condition_ttl: 20.seconds do
+        if params[:poll]
+          Poll.with_associations.find_by!(slug: params[:poll])
+        else
+          Poll.with_associations.joins(:domains).find_by!(domains: {domain: request.host})
+        end
+      end
     end
 end
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,7 +1,9 @@
 class Poll < ApplicationRecord
   has_one_attached :logo
   has_one_attached :share_image
+  has_many :domains, foreign_key: :slug, primary_key: :slug
   has_many :questions, dependent: :delete_all
   accepts_nested_attributes_for :questions, reject_if: ->(q) { q['field_name'].blank? && q['question'].blank? }, allow_destroy: :true
-  has_many :answers, through: :questions
+  has_many :answers, through: :questions, dependent: :delete_all
+  scope :with_associations, -> { with_attached_logo.with_attached_share_image.includes(questions: {answers: :image_attachment}) }
 end
