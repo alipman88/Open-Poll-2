d792a3f24fd4acd63582799e357cda91c621bbce
Aaron Lipman <alipman88@gmail.com>
2019-11-07 12:01:38 -0500

    correct admin poll params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,14 @@ class ApplicationController < ActionController::Base
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_poll
-      key = "#{ request.host }/#{ params[:poll] }"
+      key = "#{ request.host }/#{ params[:poll_id] || 0 }-#{ params[:poll] || 'id' }"
       Rails.cache.delete(key) if Rails.cache.read(key).nil?
 
       @poll = Rails.cache.fetch key, expires_in: 10.minutes, race_condition_ttl: 20.seconds do
         if params[:poll]
           Poll.with_associations.find_by!(slug: params[:poll])
+        elsif params[:poll_id]
+          Poll.with_associations.find(params[:poll_id])
         else
           Poll.with_associations.joins(:domains).find_by!(domains: {domain: request.host})
         end
